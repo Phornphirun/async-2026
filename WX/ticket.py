4ed3da8e4eee7c4f7c2a3a145d31a4e5f49c873a
Phornphirun <6710301020@cdti.ac.th>
2026-06-17 10:34:45 +0700

    edit file ticket

--- WX/ticket.py
+++ WX/ticket.py
@@ -1,9 +1,14 @@
 import sys
 
 def calculate_ticket_price(age):
-    # --- เขียนโค้ดของนักเรียนในส่วนนี้ / Write your code here ---
-    pass
-    # --------------------------------------------------------
+    if age < 12:
+        return 120
+    elif 12 <= age <= 60 :
+        return 200
+    elif age > 60:
+        return 150
+    else:
+        return 150
 
 def main():
     # เปลี่ยนมาเช็ก > 1 และใช้ sys.argv[-1] เพื่อความแม่นยำใน VPL
@@ -12,7 +17,7 @@ def main():
         result = calculate_ticket_price(test_age)
         print(result)
     else:
-        test_age = 25
+        test_age = 5
         result = calculate_ticket_price(test_age)
         print(f"Age: {test_age} -> Ticket Price: {result} Baht")
 
