8707331acd30ef850add7debc3762f568f51d4d3
Phornphirun <6710301020@cdti.ac.th>
2026-07-15 10:29:30 +0700

    code

--- Week4/foodcourt_02_gather.py
+++ Week4/foodcourt_02_gather.py
@@ -1,1 +1,32 @@
 # foodcourt_02_gather.py
+import asyncio
+from time import time, ctime
+from food_utils import send_order_to_kitchen
+
+async def main():
+    MY_STUDENT_ID = "6710301042"
+    print(f"{ctime()} | --- [Task 2] Practice using gather to wait for all group orders ---")
+
+    start_time = asyncio.get_event_loop().time()
+
+    task_chicken = asyncio.create_task(
+        send_order_to_kitchen(MY_STUDENT_ID, "hainanese_chicken", "Chicken Rice")
+    )
+    task_noodle = asyncio.create_task(
+        send_order_to_kitchen(MY_STUDENT_ID, "noodle", "Wonton Noodles")
+    )
+    task_steak = asyncio.create_task(
+        send_order_to_kitchen(MY_STUDENT_ID, "steak", "Sizzling Steak")
+    )
+
+    results = await asyncio.gather(task_chicken, task_noodle, task_steak)
+
+    for result in results:
+        print(f"{ctime()} | [Pickup] Shop: {result['shop']} | Menu: {result['menu']} is ready!")
+
+    end_time = asyncio.get_event_loop().time()
+    elapsed = end_time - start_time
+    print(f"{ctime()} | Total time: {elapsed:.2f} seconds (Equals to the slowest dish).")
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- Week4/foodcourt_03_wait_first.py
+++ Week4/foodcourt_03_wait_first.py
@@ -1,1 +1,62 @@
-# foodcourt_03_wait_first.py
+import asyncio
+from time import ctime, time
+from food_utils import send_order_to_kitchen
+
+
+async def main():
+    MY_STUDENT_ID = "6710301020"
+
+    print(f"{ctime()} | --- [Task 3] Practice using wait (FIRST_COMPLETED) ---")
+
+    start_time = time()
+
+    t1 = asyncio.create_task(
+        send_order_to_kitchen(
+            MY_STUDENT_ID,
+            "hainanese_chicken",
+            "Chicken Rice Thigh"
+        )
+    )
+
+    t2 = asyncio.create_task(
+        send_order_to_kitchen(
+            MY_STUDENT_ID,
+            "noodle",
+            "Wonton Noodles"
+        )
+    )
+
+    t3 = asyncio.create_task(
+        send_order_to_kitchen(
+            MY_STUDENT_ID,
+            "steak",
+            "Sizzling Steak"
+        )
+    )
+
+    done, pending = await asyncio.wait(
+        {t1, t2, t3},
+        return_when=asyncio.FIRST_COMPLETED
+    )
+
+    winner = done.pop().result()
+
+    print(
+        f"{ctime()} | Winner served dish! Shop: {winner['shop']} | Menu: {winner['menu']}"
+    )
+
+    print(
+        f"{ctime()} | Cleaning up: Canceling {len(pending)} remaining pending orders..."
+    )
+
+    for task in pending:
+        task.cancel()
+
+    await asyncio.gather(*pending, return_exceptions=True)
+
+    print(
+        f"{ctime()} | Total waiting time for the first dish: {time()-start_time:.2f} seconds."
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
