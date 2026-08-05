fc1505352ab6aaebe5faf9851a43f270dc927448
Phornphirun <6710301020@cdti.ac.th>
2026-08-05 12:07:12 +0700

    code

--- Week6/robots.py
+++ Week6/robots.py
@@ -18,42 +18,40 @@ ROBOTS = ["robot_1", "robot_2", "robot_3"]
 
 async def reset_factory(client: httpx.AsyncClient):
     """ส่ง Request เพื่อทำการ Reset สถานะของหุ่นยนต์ทั้งหมดของรหัสนักเรียนนี้"""
-    # TODO: เติมโค้ดการส่ง POST request ไปยัง /student/{STUDENT_ID}/reset
-    pass
+    resp = await client.post(f"/student/{STUDENT_ID}/reset")
+    resp.raise_for_status()
 
 async def grab_part(client: httpx.AsyncClient, robot_id: str, part: str):
     """สั่งให้หุ่นยนต์หยิบชิ้นส่วน 1 ชิ้น"""
-
-    # TODO: เติมโค้ดส่ง POST request ไปยัง /student/{STUDENT_ID}/robot/{robot_id}/grab
-    # พร้อมแนบ JSON Payload {"part": part}
+    resp = await client.post(
+        f"/student/{STUDENT_ID}/robot/{robot_id}/grab",
+        json={"part": part},
+    )
+    resp.raise_for_status()
+    return resp.json()
 
 async def run_robot_task(client: httpx.AsyncClient, robot_id: str):
     """สั่งให้หุ่นยนต์ 1 ตัว ทำการหยิบชิ้นส่วน A, B, และ C ตามลำดับ"""
-    print(f"[Task-{robot_id}] GET {A}")
-    await asyncio.sleep(1)  
-    
-
-    # TODO: วนลูปหยิบชิ้นส่วนใน PARTS ตามลำดับเรียงกัน (Sequential inside single robot)
-    pass
+    for part in PARTS:
+        print(f"[Task-{robot_id}] GET {part}")
+        await grab_part(client, robot_id, part)
+        await asyncio.sleep(1)
 
 async def main():
     """ฟังก์ชันหลักสำหรับเริ่มการทำงานของหุ่นยนต์ทั้ง 4 ตัวแบบ Async"""
     async with httpx.AsyncClient(base_url=BASE_URL, timeout=10.0) as client:
         print("Resetting Factory...")
         await reset_factory(client)
-        
+
         start_time = time.time()
         print("Starting Async Robot Operation...")
 
-        task = [
-        asyncio.create_task(run_robot_task("robot_1")),
-        asyncio.create_task(run_robot_task("robot_2")),
-        asyncio.create_task(run_robot_task("robot_3"))
-    ]
-        await asyncio.gather(*task)
-        
-        # TODO: สั่งรัน run_robot_task ของหุ่นยนต์ทั้ง 4 ตัวพร้อมกันโดยใช้ asyncio.gather
-        
+        tasks = [
+            asyncio.create_task(run_robot_task(client, robot_id))
+            for robot_id in ROBOTS
+        ]
+        await asyncio.gather(*tasks)
+
         elapsed_time = time.time() - start_time
         print(f"Finished all tasks in {elapsed_time:.2f} seconds.")
 
